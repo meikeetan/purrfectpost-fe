b516fbb4d24d49fc72896df66b09b626f795018a
Tan Mei Kee <meikee@Tans-MacBook-Air.local>
2024-06-15 22:41:12 +0800

    updated CreatePpost.jsx, utilities

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,11 +1,98 @@
-function CreatePost (){
-    return(
+import { useState } from "react";
+import { createPost, imageUpload } from "../utilities/apis";
+
+function CreatePost() {
+  const [newPost, setNewPost] = useState({
+    caption: "",
+    images: [],
+  });
+  function handleChange(evt) {
+    if (evt.target.name === "images") {
+      setNewPost({ ...newPost, images: Array.from(evt.target.files) });
+    } else {
+      setNewPost({ ...newPost, [evt.target.name]: evt.target.value });
+    }
+  }
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+    try {
+      createPost(newPost);
+      alert('Post created!')
+      setNewPost({
+        caption: "",
+        images: [],
+      })
+      setFile(null); 
+      document.getElementById('fileInput').value = '';
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const [file, setFile] = useState();
+
+  async function handleUpload() {
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const response = await imageUpload(formData);
+      const imageUrl = response.data;
+      setNewPost({
+        ...newPost,
+        images: [...newPost.images, imageUrl],
+      });
+      if(imageUrl){
+      alert("Image attached. Please submit your form!")
+      console.log(imageUrl);}
+    } catch (error) {
+      console.error("Image uploading failed.", error);
+    }
+  }
+
+  return (
+    <div className="create-Post-container">
+    <h1>Create New Post</h1>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="fileInput">Images:</label>
+        <input
+          id="fileInput"
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <button type="button" onClick={handleUpload}>
+          Upload
+        </button>
+      </div>
+      {newPost.images.length > 0 && (
         <div>
-            <input placeholder='Caption'></input>
-            <br/>
-            <input type="file"></input>
+          <h3>Uploaded Images:</h3>
+          <ul>
+            {newPost.images.map((image, index) => (
+              <li className="uploaded-image" key={index}>{image}</li>
+            ))}
+          </ul>
         </div>
-    )
+      )}
+
+<label>
+        Caption
+        <input
+          placeholder="Caption"
+          name="caption"
+          type="text"
+          value={newPost.caption}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+);
 }
 
 export default CreatePost
--- src/utilities/apis.js
+++ src/utilities/apis.js
@@ -1,46 +1,69 @@
+import sendRequest from "./send-request";
 import axios from "axios";
 
-const baseURL = "http://localhost:5001/api";
-const api = axios.create({
-    baseURL,
-    withCredentials: true,
-  });
+const BASE_URL = "http://localhost:5001/api";
+// const api = axios.create({
+//     baseURL,
+//     withCredentials: true,
+//   });
 
-export const userSignup = async (userData) => {
-    try {
-        const response = await api.post(`${baseURL}/users`, userData);
-        console.log("Signup Response:", response);
+// export const userSignup = async (userData) => {
+//     try {
+//         const response = await api.post(`${baseURL}/users`, userData);
+//         console.log("Signup Response:", response);
    
-      const token = response.data.token;
-      if (!token) {
-        throw new Error("Token not found in signup response headers");
-      }
-      localStorage.setItem("token", token);
-      console.log(
-        "Token stored in local storage:",
-        localStorage.getItem("token")
-      );
-      const user = response.data.user;
-      return { token, user };
-    } catch (error) {
-      console.error("Signup failed:", error);
-      throw new Error("Signup failed");
-    }
-  }
+//       const token = response.data.token;
+//       if (!token) {
+//         throw new Error("Token not found in signup response headers");
+//       }
+//       localStorage.setItem("token", token);
+//       console.log(
+//         "Token stored in local storage:",
+//         localStorage.getItem("token")
+//       );
+//       const user = response.data.user;
+//       return { token, user };
+//     } catch (error) {
+//       console.error("Signup failed:", error);
+//       throw new Error("Signup failed");
+//     }
+//   }
 
-  export const userLogin = async (userData) => {
-    try {
-      console.log(userData)
-      const response = await api.post(`/users/login`, userData);
-      console.log("Login Response:", response);
-      const token = response.data.token;
-      const user = response.data.user;
-      localStorage.setItem("token", token);
-      return { token, user };
-    } catch (error) {
-      console.error("Login failed:", error);
-      throw new Error("Login failed");
-    }
-  };
+export function userSignup(userInput) {
+  return sendRequest(`${BASE_URL}/users`, 'POST', userInput)
+}
+
+  // export const userLogin = async (userData) => {
+  //   try {
+  //     console.log(userData)
+  //     const response = await api.post(`/users/login`, userData);
+  //     console.log("Login Response:", response);
+  //     const token = response.data.token;
+  //     const user = response.data.user;
+  //     localStorage.setItem("token", token);
+  //     return { token, user };
+  //   } catch (error) {
+  //     console.error("Login failed:", error);
+  //     throw new Error("Login failed");
+  //   }
+  // };
+
+  export function userLogin(userInput) {
+    return sendRequest(`${BASE_URL}/users/login`, 'POST', userInput)
+}
+
+export function createPost(userInput) {
+  return sendRequest(`${BASE_URL}/post/create`, 'POST', userInput)
+}
+
+export async function imageUpload(formData){
+  const response = await axios.post(
+      `${BASE_URL}/upload/image`,
+      formData
+    )
+    return response;
+}
+
+
 
 
